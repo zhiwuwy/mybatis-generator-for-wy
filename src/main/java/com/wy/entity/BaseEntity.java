5c743c4f7123a10765ad45f37b6ca3752d9dc185
NBdiMAN <907645377@qq.com>
2020-08-15 02:07:17 +0800

    修正

---
+++ src/main/java/com/wy/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.wy.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author wangyang
+ * @date 2020/8/15 1:39
+ * @description:
+ */
+public class BaseEntity implements Serializable {
+}
--- src/main/java/com/wy/my/mapper/Test.java
+++
@@ -1,8 +0,0 @@
-package com.wy.my.mapper;
-
-/**
- * @Author: Wang Yang
- * @Date: 2020/6/7 10:50
- */
-public class Test {
-}
